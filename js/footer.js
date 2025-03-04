299851aa88d4b90d55f8c6d1bdd1477dff13a155
Jonathan <20230009@ricaldone.edu.sv>
2025-03-03 20:53:38 -0600

    Subir proyecto

---
+++ js/footer.js
@@ -0,0 +1,36 @@
+function generarFooterIndex()
+{
+    const footerIndex = `      <div class="container-fluid footer custom-footer text-white mt-5 pt-5 px-0 position-relative overlay-top">
+      <div class="row mx-0 pt-5 px-sm-3 px-lg-5 mt-4">
+          <div class="col-lg-3 col-md-6 mb-5">
+              <h4 class="text-white text-uppercase mb-4" style="letter-spacing: 3px;">Contacto</h4>
+              <p><i class="fa fa-map-marker-alt mr-2"></i>Instituto Técnico Ricaldone, San Salvador</p>
+              <p><i class="fa fa-phone-alt mr-2"></i>+7070-8120</p>
+              <p class="m-0"><i class="fa fa-envelope mr-2"></i>20230009@ricaldone.edu.sv</p>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+              <h4 class="text-white text-uppercase mb-4" style="letter-spacing: 3px;">Redes Sociales</h4>
+              <p>@jincld</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mb-5">
+
+        </div>
+
+          <div class="col-lg-3 col-md-6 mb-5">
+              <h4 class="text-white text-uppercase mb-4" style="letter-spacing: 3px;">Horario Laboral</h4>
+              <div>
+                  <h6 class="text-white text-uppercase">Lunes - Viernes</h6>
+                  <p>8.00 AM - 5.00 PM</p>
+                  <h6 class="text-white text-uppercase">Sábado</h6>
+                  <p>2.00 PM - 8.00 PM</p>
+              </div>
+          </div>
+      </div>
+      <div class="container-fluid text-center text-white border-top mt-4 py-4 px-sm-3 px-md-5" style="border-color: rgba(256, 256, 256, .1) !important;">
+          <p class="mb-2 text-white">Copyright Jonathan Orellana. Derechos Reservados.</p>
+      </div>
+  </div>`;
+
+  return footerIndex;
+}
